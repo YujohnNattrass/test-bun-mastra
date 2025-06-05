13af1151ca4eeb43a48457521d1c002aec51f2ab
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-06-04 21:46:01 -0700

    1
    6
    
    4
    
    somethign

--- src/mastra/workflows/weather-workflow.ts
+++ src/mastra/workflows/weather-workflow.ts
@@ -96,7 +96,7 @@ const fetchWeather = createStep({
       throw new Error('Input data not found');
     }
 
-    console.log('hi4');
+    console.log('hi71232');
 
     const geocodingUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(inputData.city)}&count=1`;
     const geocodingResponse = await fetch(geocodingUrl);
